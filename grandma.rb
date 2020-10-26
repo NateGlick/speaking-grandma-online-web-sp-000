a498fd6a0e36b6f50d5d0fcc03ef647075be537e
Nathan Glick <nateleighglick@hotmail.com>
2020-10-26 09:53:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(" ")
-  if "I LOVE YOU GRANDMA" == "I LOVE YOU TO PUMPKIN"
+  if "I LOVE YOU GRANDMA" == "I LOVE YOU TOO PUMPKIN"
   end
 end
 
